405258878872a49d71c1e6fbc34376a6e88e9a20
Nuriev Azat <aznuri@yandex.ru>
2024-01-14 15:06:45 +0500

    ES 1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
+import csv
+import os
+
+
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +19,36 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            self.__name = new_name[:10]
+        else:
+            self.__name = new_name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('src/items.csv', newline='') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    print(row)
+                    Item.all.append(Item(row['name'], row['price'], row['quantity']))
+
+    @staticmethod
+    def string_to_number(str_):
+        return int(float(str_))
+
+
+
 
     def apply_discount(self):
         """
